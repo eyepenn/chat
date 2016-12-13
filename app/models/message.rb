0aaa438c821ad40f6bdeb1734140c333bdc995b8
Andrew Clark <eyepenn@yahoo.com>
2016-12-13 03:35:53 +0000

    css updates and format date

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
 class Message < ActiveRecord::Base
-	validates :person, presence: true, length: { maximum: 25, minimum: 1 }
-	validates :dialog, presence: true, length: { maximum: 140, minimum: 1 }
+  validates :person, presence: true, length: { maximum: 25, minimum: 1 }
+  validates :dialog, presence: true, length: { maximum: 140, minimum: 1 }
 end
