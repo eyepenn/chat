6cc398bf35fd61e4ce9ec208979f847eac4a8834
Andrew Clark <eyepenn@yahoo.com>
2016-12-13 21:14:35 +0000

    JavaScript complete

---
+++ app/assets/javascripts/messages.js
@@ -0,0 +1,52 @@
+$(function() {
+
+    $.get("/messages").success( function( data ) {
+      var htmlString = '';
+       $.each(data, function(index,  message) {
+        var day = new Date(message.created_at).toLocaleDateString();
+        var time = new Date(message.created_at).toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3");
+        var divElement = '<div class="' + message.person + '"><span class="timestamp">' + day + " - " + time + '</span><br />' + message.dialog + '</div>';
+        htmlString += divElement;
+      });
+      var divMessages = $('#messages');
+      divMessages.html(htmlString);
+    });
+
+    $('#buyer-form').submit(function(event) {
+      event.preventDefault();
+      var textbox = $('.buyer_new_msg');
+       var payload = {
+        message: {
+          person: 'buyer',
+          dialog: textbox.val()
+        }
+      };
+      $.post("/messages", payload).success(function(message) {
+        var day = new Date(message.created_at).toLocaleDateString();
+        var time = new Date(message.created_at).toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3");
+        var divElement = '<div class="' + message.person + '"><span class="timestamp">' + day + " - " + time + '</span><br />' + message.dialog + '</div>';
+        var divMessages = $('#messages');
+        divMessages.append(divElement);
+        textbox.val('');
+      });
+    });
+
+    $('#seller-form').submit(function(event) {
+      event.preventDefault();
+      var textbox = $('.seller_new_msg');
+       var payload = {
+        message: {
+          person: 'seller',
+          dialog: textbox.val()
+        }
+      };
+      $.post("/messages", payload).success(function(message) {
+        var day = new Date(message.created_at).toLocaleDateString();
+        var time = new Date(message.created_at).toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3");
+        var divElement = '<div class="' + message.person + '"><span class="timestamp">' + day + " - " + time + '</span><br />' + message.dialog + '</div>';
+        var divMessages = $('#messages');
+        divMessages.append(divElement);
+        textbox.val('');
+      });
+    });
+  });
