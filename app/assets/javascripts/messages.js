18674090e2ae0fac75abfc4918db3125d5419589
Andrew Clark <eyepenn@yahoo.com>
2016-12-16 19:39:45 +0000

    DRY JS for submit

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -11,13 +11,12 @@ $(function() {
       $('#messages').html(htmlString);
     });
 
-    $('#buyer-form').submit(function(event) {
-      event.preventDefault();
-      var textbox = $('.buyer_new_msg');
+    function form_submit(person) {
+      var textbox = $('.' + person + '_new_msg');
       if (textbox.val() != '') {
        var payload = {
         message: {
-          person: 'buyer',
+          person: person,
           dialog: textbox.val()
         }
       };
@@ -28,26 +27,16 @@ $(function() {
         $('#messages').append(divElement);
         textbox.val('');
       });
-    }
+      }
+    };
+
+    $('#buyer-form').submit(function(event) {
+      event.preventDefault();
+      form_submit('buyer')
     });
 
     $('#seller-form').submit(function(event) {
       event.preventDefault();
-      var textbox = $('.seller_new_msg');
-      if (textbox.val() != '') {
-       var payload = {
-        message: {
-          person: 'seller',
-          dialog: textbox.val()
-        }
-      };
-      $.post("/messages", payload).success(function(message) {
-        var day = new Date(message.created_at).toLocaleDateString();
-        var time = new Date(message.created_at).toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3");
-        var divElement = '<div class="' + message.person + '"><span class="timestamp">' + day + " - " + time + '</span><br />' + message.dialog + '</div>';
-        $('#messages').append(divElement);
-        textbox.val('');
-      });
-      }
+      form_submit('seller')
     });
   });
